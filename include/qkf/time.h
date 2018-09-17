fff3afa66373880f1a1825971674648f95c8d586
romandion <romandion@163.com>
2018-09-17 18:17:32 +0800

    impl time

--- include/qkf/time.h
+++ include/qkf/time.h
@@ -8,11 +8,41 @@
 
 __BEGIN_DECLS
 
+/*
+	由于是同一天，所以没有必要在时间中保存日期的信息，将日期单独是个明智的处理。
+	对于时间来说，每天3600*24 = 86400秒，是个常量。
+
+	这里年月日，都是表示本地时间。
+
+	关于time.h中几个重要函数解释。
+	time(NULL) ；返回的值，同本地时间到EPOCH的秒数相同
+	mktime(struct tm)；也是基于本地时间进行计算
+	除非特别指出，这里的时间秒数，都是基于本地时间
+
+    32位的linux的time_t是32位，windows和64位linux都是64位。请注意在32位linux下的溢出问题。    
+*/
+
 QKFAPI int64_t  qkf_now_usec() ;
 QKFAPI int64_t  qkf_now_msec() ;
 
 QKFAPI bool  qkf_next_nsec(struct timespec * ts , int64_t nsec) ;
 
+//可读性比较强的日期
+typedef struct __st_date{
+    int year ;              //年    2018
+    int month ;             //月    1...12
+    int day ;               //日    1...31
+    int wday ;              //周几  1...7
+}date_t ;
+
+QKFAPI  bool qkf_time_to_date(time_t t , date_t * date) ;
+QKFAPI  bool qkf_time_from_date(time_t * t , const date_t * date) ;
+
+/*
+    解析日期时间。支持格式为"YYYY-MM-DD HH:MM:SS.mmm"或者"YYYYMMDD HH:MM:SS.mmm" 或者"YYYY/MM/DD HH:MM:SS.mmm"
+    如果日期有分隔符，那么可以不补充0
+*/
+QKFAPI bool qkf_parse_time(const char * str , int&year , int& month , int& day , int& hour , int& minute , int& second , int& msec) ;
 
 __END_DECLS
 
--- qkf/time.cpp
+++ qkf/time.cpp
@@ -1,5 +1,6 @@
 
 #include "qkf/time.h"
+#include <string.h>
 
 int64_t  qkf_now_usec()
 {
@@ -42,7 +43,146 @@ bool  qkf_next_nsec(struct timespec * ts , int64_t nsec)
         nsec = ns ;
 
     ts->tv_sec = tv.tv_sec + sec ;
-    ts->tv_nsec = nsec ;
+    ts->tv_nsec = (long)nsec ;
     return true ;
+}
+
+bool qkf_time_to_date(time_t t , date_t * date)
+{
+    if(date == NULL)
+        return false ;
+
+    if(t == 0)
+        t = ::time(NULL) ;
+
+    struct tm tm ;
+    if(::localtime_r(&t , &tm) == NULL)
+        return false ;
+
+    date->year = tm.tm_year + 1900 ;
+    date->month = tm.tm_mon + 1 ;
+    date->day = tm.tm_mday ;
+    date->wday = tm.tm_wday ;
+    if(date->wday == 0)
+        date->wday = 7 ;
+
+    return true ;
+}
+
+bool qkf_time_from_date(time_t * t , const date_t * date)
+{
+    if(t == NULL || date == NULL)
+        return false ;
+
+    struct tm tm ;
+    ::memset(&tm , 0 , sizeof(tm)) ;
+
+    tm.tm_year = date->year - 1900 ;
+	tm.tm_mon = date->month - 1 ;
+	tm.tm_mday = date->day ;
+
+	tm.tm_hour = 0 ;
+	tm.tm_min = 0 ;
+	tm.tm_sec = 0 ;	
+
+	*t = ::mktime(&tm) ;
+    return true ;
+}
+
+bool qkf_parse_time(const char * str , int&year , int& month , int& day , int& hour , int& minute , int& second , int& msec) 
+{
+	year = month = day = hour = minute = second = msec = 0 ;
+	if(str == NULL)
+		return false ;
+
+	const char * pchar = str ;
+	int stage = 0 ;
+	char ch = 0 , buffer[32] = {'\0'};
+	int buflen = 0 ;
+	do{
+		ch = pchar[0] ;
+
+		if(stage >= 0 && stage <= 2)
+		{
+			//日期，'-','/'以及最后的空格作为分隔符，如果没有分隔符，按长度分隔
+			bool finish = false , seperate = false;
+			if(ch >= '0' && ch <= '9')
+			{
+				buffer[buflen++] = ch ;
+			}
+			else
+			{
+				seperate = finish = true ;		//有分隔符，那么同时表示结束
+			}
+
+			if(finish == false)
+			{
+				if(stage == 0)
+				{					
+					if(buflen >= 5) 
+					{
+						finish = true ; 
+						buflen = 4 ;
+					}
+				}
+				else if(stage == 1 || stage == 2)
+				{
+					if(buflen >= 3) 
+					{
+						finish = true ; 
+						buflen = 2 ;
+					}
+				}				
+			}
+
+			if(finish == true)
+			{
+				buffer[buflen] = '\0' ;
+				switch(stage)
+				{
+				case 0 : year = atoi(buffer) ; break ;
+				case 1 : month = atoi(buffer) ;  break ;
+				case 2 : day = atoi(buffer) ; break ;
+				default :break ;
+				}
+
+				buflen = 0 ;
+				if(seperate == false)
+				{
+					buffer[buflen++] = ch ;
+				}
+				stage++ ;				
+			}
+
+		}
+		else if(stage >= 3 && stage <= 6)
+		{
+			//时间，必须以":"分隔，中间可能没有0
+			if(ch >= '0' && ch <= '9')
+			{
+				buffer[buflen++] = ch ;
+			}
+			else if(ch == ':' || ch == '.' || ch == '\0')
+			{
+				buffer[buflen] = '\0' ;
+				switch(stage)
+				{
+				case 3 : hour = atoi(buffer) ; break ;
+				case 4 : minute = atoi(buffer) ; break ;
+				case 5 : second  = atoi(buffer) ; break ;
+				case 6 : msec = atoi(buffer) ; break ;
+				default : break ;
+				}
+				
+				buflen = 0 ;
+				stage++ ;
+			}
+		}
+
+		pchar ++ ;	
+		
+	} while(ch != 0) ;
+
+	return true ;	
 }
 
